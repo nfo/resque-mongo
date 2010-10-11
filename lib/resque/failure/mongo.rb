f0754e27b77fe5a0b3664b1656f80097d5669a86
Gheorghita Catalin Bordianu <catalin@silentale.com>
2010-10-11 01:43:50 -0400

    Modified the search, so that each failed job that is returned matches each regexp defined by each term of the search string by at least one relevant field

--- lib/resque/failure/mongo.rb
+++ lib/resque/failure/mongo.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 module Resque
   module Failure
     # A Failure backend that stores exceptions in Mongo. Very simple but
@@ -29,26 +31,32 @@ module Resque
       # Looks for failed jobs who match a particular search string
       def self.search_results(start = 0, count = 1, squery)
         start, count = [start, count].map { |n| Integer(n) }
-        search_res = [];
+        res = Set.new
         
+        # If the search query is nil or empty, return an empty array
         if squery.nil? || squery.empty?
-          return search_res
+          return []
         end
         
-        Resque.mongo_failures.find({}, {:fields => {"backtrace" => 0, "failed_at" => 0}}).sort([:natural, :desc]).skip(start).limit(count).to_a.each do |failure|
-            failure.each_key do |key|
-              match = false
-              squery.split.each do |term|
-                if failure[key].to_s =~ /#{term}/i
-                  match = true
-                  break
-                end
-              end
-              if match
-                search_res << failure
-                break
-              end
+        # For each search term, retrieve the failed jobs that contain at least one relevant field matching the regexp defined by that search term
+        squery.split.each do |sterm|
+          _sres = Resque.mongo_failures.find({"$or" => [ {"exception" => /#{sterm}/i}, {"error" => /#{sterm}/i}, {"payload.class" => /#{sterm}/i}, \
+            {"payload.args" => /#{sterm}/i}, {"worker" => /#{sterm}/i}, {"queue" => /#{sterm}/i} ] }, {:fields => {"backtrace" =>0, "failed_at" => 0}}).sort([:natural, :desc]).to_a
+            
+            # If the set was empty, merge the first results, else intersect it with the current results
+            if res.empty?
+              res.merge(_sres)
+            else
+              res = res & _sres
             end
+        end
+        
+        # search_res will be an array containing 'count' values, starting with 'start', sorted in descending order
+        search_res = res.to_a[start, count]
+        
+        # If start was greater than the size of res, [] returned nil, so this check if needed
+        if search_res.nil?
+          search_res = []
         end
         
         search_res.size == 1 ? search_res.first : search_res
