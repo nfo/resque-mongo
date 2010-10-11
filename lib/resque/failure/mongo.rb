419cf81c8ec0b0b175e37099c191cae239c75810
Gheorghita Catalin Bordianu <catalin@silentale.com>
2010-10-11 14:09:42 -0400

    Some more cosmetic changes

--- lib/resque/failure/mongo.rb
+++ lib/resque/failure/mongo.rb
@@ -30,11 +30,11 @@ module Resque
       
       # Looks for failed jobs who match a particular search string
       def self.search_results(query, start = 0, count = 1)
-        start, count = [start, count].map { |n| Integer(n) }
-        set_results = Set.new
-        
         # If the search query is nil or empty, return an empty array
         return [] if query.nil? || query.empty?
+        
+        start, count = [start, count].map { |n| Integer(n) }
+        set_results = Set.new
         
         # For each search term, retrieve the failed jobs that contain at least one relevant field matching the regexp defined by that search term
         query.split.each do |term|
@@ -50,20 +50,18 @@ module Resque
             {:fields => {"backtrace" =>0, "failed_at" => 0}}
           ).sort([:natural, :desc]).to_a
             
-            # If the set was empty, merge the first results, else intersect it with the current results
-            if set_results.empty?
-              set_results.merge(partial_results)
-            else
-              set_results = set_results & partial_results
-            end
+          # If the set was empty, merge the first results, else intersect it with the current results
+          if set_results.empty?
+            set_results.merge(partial_results)
+          else
+            set_results = set_results & partial_results
+          end
         end
         
         # search_res will be an array containing 'count' values, starting with 'start', sorted in descending order
         search_results = set_results.to_a[start, count]
         
-        # If start was greater than the size of res, [] returned nil, so this check if needed
-        search_results ||= []
-        search_results
+        search_results || []
       end
 
       def self.clear
